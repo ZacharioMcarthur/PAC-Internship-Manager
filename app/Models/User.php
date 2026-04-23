08b13f990e191e0c0a3b929c009281fabed2fe5d
ZacharioMcarthur <nascimentozachario@gmail.com>
2026-04-23 14:15:51 +0100

    Création des modeles Presence/Activite et configuration des relations

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,14 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function presences()
+    {
+        return $this->hasMany(Presence::class);
+    }
+
+    public function activites()
+    {
+        return $this->hasMany(Activite::class);
+    }
 }
